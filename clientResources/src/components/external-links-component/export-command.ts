ca820e861ec503f2ae238da69251b547f2368ee7
Grzegorz Wiechec <gwiechec@o2.pl>
2022-02-19 12:22:24 +0100

    Fix export command

--- clientResources/src/components/external-links-component/export-command.ts
+++ clientResources/src/components/external-links-component/export-command.ts
@@ -1,11 +1,11 @@
 import { React_Command } from "./React_Command";
 
 class ExportCommand extends React_Command { //TODO: LINKS move to commands
-    label: "Export"; //TODO: LINKS resources
-    category: "context";
-    iconClass: "epi-iconDownload epi-icon--medium";
+    label: string = "Export"; //TODO: LINKS resources
+    category: string = "context";
+    iconClass: string = "epi-iconDownload epi-icon--medium";
 
-    _execute() {
+    execute() {
         const link = window.document.createElement("a");
         link.setAttribute("href", "/ExternalLinks/export"); //TODO: LINKS fix URL to export
         link.setAttribute("download", "export.csv");
