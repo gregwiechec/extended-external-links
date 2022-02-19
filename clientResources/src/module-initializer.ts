f70277e4065e5cd4f1cec2bf963343f56e94ff80
Grzegorz Wiechec <gwiechec@o2.pl>
2022-02-19 12:29:00 +0100

    Convert module initializer to class

--- clientResources/src/module-initializer.ts
+++ clientResources/src/module-initializer.ts
@@ -1,10 +1,19 @@
-import declare from "dojo/_base/declare";
-import _Module from "epi/_Module";
 import dependency from "epi/dependency";
 
 // Edit mode dojo initializer for external links module
 // File is used as an entry point in build
-export default declare([_Module], {
+export default class ModuleInitializer {
+    initialize(): void {
+        // register new view
+        const hashWrapper = dependency.resolve<epi.HashWrapper>("epi.shell.HashWrapper");
+        const contextService = dependency.resolve<epi.ContextService>("epi.shell.ContextService");
+        contextService.registerRoute("external-links", function (context, callerData) {
+            hashWrapper.onContextChange(context, callerData);
+        });
+    }
+}
+
+/*export default declare([_Module], {
     initialize: function () {
         this.inherited(arguments);
 
@@ -15,4 +24,4 @@ export default declare([_Module], {
             hashWrapper.onContextChange(context, callerData);
         });
     }
-});
+});*/
